c6f608e30b22915ef411892780d63a0d736548d5
andyjin777 <MacBook-JIN@JINui-MacBook-Pro.local>
2016-08-03 09:08:02 +0900

    check_push_mac_add

---
+++ push.js
@@ -0,0 +1,19 @@
+var arp = require('node-arp');
+var network = require('network');
+
+network.get_private_ip(function(err, ip) {
+	arp.getMAC((err||ip), function(err, mac) {
+
+		var my_mac = mac;
+
+		setInterval(function() {
+			arp.getMAC((err||ip), function(err, mac) {
+		    	if (!err) {
+		    		if(my_mac != mac) {
+		    			console.log("CHANGE!!");
+		    		}
+		    	}
+		    });
+		}, 1000);
+	});
+})
